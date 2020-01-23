9ffdf09baa18eb858f158d8e0cd388012b1d345d
LuisLimaDev <56639074+LuisLimaDev@users.noreply.github.com>
2020-01-23 11:50:44 -0300

    Create bancoInfos.js

---
+++ bancoInfos.js
@@ -0,0 +1,54 @@
+// bancoInfos.js
+
+fotos = [
+	{
+		"link": "https://scontent-gru2-1.xx.fbcdn.net/v/t1.0-9/29389353_1747684955290451_247342102810198016_n.jpg?_nc_cat=111&_nc_ohc=_MLNA8TJtUAAX_f8E_Y&_nc_ht=scontent-gru2-1.xx&oh=0023ceda9dc439d0dbf3650d8a64aa30&oe=5ED7A8FB",
+		"titulo": "na trance"
+	},
+	{
+		"link": "https://scontent-gru1-1.xx.fbcdn.net/v/t1.0-0/p640x640/57272169_2184035358377201_3969651913061826560_o.jpg?_nc_cat=104&_nc_ohc=vVM_DIbhrYMAX9I_hZw&_nc_ht=scontent-gru1-1.xx&_nc_tp=1002&oh=1eb845ca40c428cb97fa38116e94333e&oe=5ED3C3B1",
+		"titulo": "dos brother"
+	},
+	{
+		"link": "https://scontent-gru2-2.xx.fbcdn.net/v/t1.0-0/p640x640/44621128_1116610401849905_688331847066714112_o.jpg?_nc_cat=105&_nc_ohc=jYAle9tCKskAX9r3cTG&_nc_ht=scontent-gru2-2.xx&_nc_tp=1002&oh=d236defb3c644cf24c8c2dcfb3a5b9f5&oe=5E9A4BE2",
+		"titulo": "brinquedoteca da facul"
+	},
+	{
+		"link": "https://scontent-gru1-1.xx.fbcdn.net/v/t1.0-9/p960x960/81167632_2784522128273390_8868970315049336832_o.jpg?_nc_cat=104&_nc_ohc=Ho7rtlqfkEkAX82vYOM&_nc_ht=scontent-gru1-1.xx&_nc_tp=1002&oh=9294a16aae914443700677285f89a687&oe=5ED17238",
+		"titulo": "férias"
+	},
+	{
+		"link": "https://scontent-gru2-2.xx.fbcdn.net/v/t31.0-8/s960x960/15774840_1302479616477656_4942480968212075867_o.jpg?_nc_cat=106&_nc_ohc=9IXP6_EySmAAX-_GadS&_nc_ht=scontent-gru2-2.xx&_nc_tp=1002&oh=d6d94d676e7b918d6d4dee2c12cb891d&oe=5E951D32",
+		"titulo": "ponto NÂO turistico"
+	},
+	{
+		"link": "https://scontent-gru2-1.xx.fbcdn.net/v/t31.0-8/s960x960/15590914_1291330714259213_2650179522690251753_o.jpg?_nc_cat=107&_nc_ohc=r_PIgGRBMUsAX9hyn2N&_nc_ht=scontent-gru2-1.xx&_nc_tp=1002&oh=e213f2108e29c80405ecd4e757d29525&oe=5EA5A9B4",
+		"titulo": "é charuto"
+	}
+];
+musicas = [
+	{
+		"link": "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/339582422&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true",
+		"titulo": "Doutor em samba (Remix)",
+		"capa": "https://i1.sndcdn.com/artworks-000239972789-po1jc9-t500x500.jpg",
+		"autoria": "Mário Reis"
+	},
+	{
+		"link": "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/275246920&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true",
+		"titulo": "Bom dia (Original Mix)",
+		"capa": "https://i1.sndcdn.com/artworks-000173183094-4h3j74-t500x500.jpg",
+		"autoria": "Luis Lima"
+	},
+	{
+		"link": "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/275246917&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true",
+		"titulo": "Ride (Luis Lima Remix)",
+		"capa": "https://i1.sndcdn.com/artworks-000173183094-4h3j74-t500x500.jpg",
+		"autoria": "21 Pilots"
+	},
+	{
+		"link": "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/275246913&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
+		"titulo": "Mulher elétrica (Luis Lima Remix)",
+		"capa": "https://i1.sndcdn.com/artworks-000173183094-4h3j74-t500x500.jpg",
+		"autoria": "Racionais Mc's"
+	}
+]
